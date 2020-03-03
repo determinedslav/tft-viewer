205e3757f47a5826dbc2b54747e87131115c30fd
DeterminedSlav <slavimitev10@gmail.com>
2020-03-03 22:10:34 +0200

    removed comented code

--- src/pages/Match.js
+++ src/pages/Match.js
@@ -1,4 +1,4 @@
-import React, {/* useEffect, useState */} from 'react';
+import React from 'react';
 import {useSelector, useDispatch} from "react-redux";
 import LoadingSplash from '../components/LoadingSplash'
 import PlayerCard from '../components/PlayerCard'
