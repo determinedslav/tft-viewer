26953ce8bec87d53868442aad072154a3f8e1ac7
DeterminedSlav <slavimitev10@gmail.com>
2020-03-04 16:06:28 +0200

    added comments

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,13 +1,20 @@
 export default {
+    //Base API constants
     protocol: "https://",
-    europe: "europe",
     apiLink: ".api.riotgames.com/tft/",
     key: "?api_key=",
     keyValue: "RGAPI-14d3be1d-0b07-494a-b210-a0a31aeece27",
+
+    //API server region constant
+    europe: "europe",
+
+    //GET for each API endpoint
     nameByName: "summoner/v1/summoners/by-name/",
     statsBySummonerId: "league/v1/entries/by-summoner/",
     matchesByPuuid: "match/v1/matches/by-puuid/",
+    matchByMatchId: "match/v1/matches/",
+
+    //Extra params needed for matches API
     matchesParams: "/ids?count=10&api_key=",
-    matchByMatchId: "match/v1/matches/"
 
 }
